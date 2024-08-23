8ab11dcf95d6c725b5cf92bb10d7ec81db45abe4
thansoe-2000 <tt6289216@gmail.com>
2024-08-23 08:37:22 -0700

    record project/ township-crud

--- myapp/forms.py
+++ myapp/forms.py
@@ -2,29 +2,41 @@ from .models import *
 from django import forms
 from django.forms import ModelForm
 
-class TownshipForm(ModelForm):
+class TownshipForm(forms.ModelForm):
     class Meta:
         model = Township
         fields = '__all__'
         labels = {
-            'name':'Name'
+            'name':'မြို့နယ် နာမည်'
         }
         widget = {
-            'name':forms.TextInput(attrs=({'placeholder':'နာမေ ကို ရိုက်ထည့်ပါ'})),
+            'name':forms.TextInput(attrs=({'placeholder':'မြို့နယ်နာမေ ထည့်ပါ။'})),
         }
+      
         
-class VillageForm(ModelForm):
+class VillageForm(forms.ModelForm):
     class Meta:
         model = Village
         fields = '__all__'
         labels = {
-            'schools':'Schools',
+            'township':'မြို့နယ်',
+            'schools':'ကျောင်း',
+            'houses':'အိမ်',
+            'populations':'လူဦးရေ',
+            'monestaries':'ဘုန်းကြီးကျောင်း',
+            'name':'ရွာနာမည်',
         }
-        widget = {
-            'schools':forms.TextInput(attrs={'placeholder':'ကျောင်းအရီအတွက် ရိုက်ထည့်ပါ။'})
-        }
-        def __init__(self, *args, **kwargs):
-            super(VillageForm, self).__init__(*args, **kwargs)
+            
+        widgets = {
+         
+         'schools':forms.NumberInput(attrs=({'placeholder':'စာသင်ကျောင်းအရီအတွက် ရိုက်ထည့်ပါ'})),
+         'houses':forms.NumberInput(attrs=({'placeholder':'အိမ် အရီအတွက် ရိုက်ထည့်ပါ'})),
+         'populations':forms.NumberInput(attrs=({'placeholder':'လူဦးရေ အရီအတွက် ရိုက်ထည့်ပါ'})),
+         'monestaries':forms.NumberInput(attrs=({'placeholder':'ဘုန်းကြီးကျောင်းအရီအတွက် ရိုက်ထည့်ပါ'})),
+         'name':forms.TextInput(attrs=({'placeholder':'ရွာ နာမေ ရိုက်ထည့်ပါ'})),
+         
+      }
+        
        
       
     
